cdff24d2b791ed817a928aaeea21db66ffc749fb
nithinwebdev <nithin.thewebdev@gmail.com>
2021-02-04 14:56:32 +0530

    added directory and menu components with sass styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Directory from "./components/Directory/Directory";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Directory />
+    </div>
+  );
 }
 
 export default App;
