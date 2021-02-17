28e6b2aa30685edf18ae3af24dc37b4531359c10
nithinwebdev <nithin.thewebdev@gmail.com>
2021-02-17 23:29:07 +0530

    added shopping cart quantity

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -5,13 +5,29 @@ import { toggleCartHidden } from "../../redux/cart/cartActions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./CartIconSASS.scss";
 // we get the toggleCartHidden action as props
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
-  </div>
-);
+const CartIcon = ({ toggleCartHidden, cartItems, itemCount }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  );
+};
+
+// access the cartItems state
+//we can destructure the cartItems as ({cart:{cartItems}})
+const mapStateToProps = (state) => {
+  console.log("I am being Called!");
+  return {
+    itemCount: state.cart.cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    ),
+  };
+};
+// dispatch the action to toggle the cart
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
