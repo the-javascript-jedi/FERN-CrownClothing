6d7621e1d6a7551a7e6a4b0f0e79d7fb9375b201
nithinwebdev <nithin.thewebdev@gmail.com>
2021-02-19 09:34:49 +0530

    added redux-persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,23 @@
 // combines all our other states in rootReducer
 import { combineReducers } from "redux";
-
+// persistReducer - will persist our reducer
+import { persistReducer } from "redux-persist";
+//storage-using local storage, (to use session storage, search docs for package path)
+import storage from "redux-persist/lib/storage";
+//reducers
 import userReducer from "../redux/user/userReducer";
 import cartReducer from "../redux/cart/cartReducer";
-
-export default combineReducers({
+// PersistConfig
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  //whitelist-array containing string name  any of reducer we want to store
+  whitelist: ["cart"],
+};
+const rootReducer = combineReducers({
+  // user state is persisited by firebase
   user: userReducer,
   cart: cartReducer,
 });
+// export the persistReducer
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
+// persistStore will cache our browser store
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middlewares))
 );
-
-export default store;
+// persistor - persisted version of our store
+export const persistor = persistStore(store);
+// export default { store, persistor };
